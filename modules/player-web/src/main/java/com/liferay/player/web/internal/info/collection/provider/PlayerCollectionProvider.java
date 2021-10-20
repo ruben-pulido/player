e8a6efc83e627001cf8ee9ec5d7697500e33c177
Rubén Pulido <ruben.pulido@liferay.com>
2021-10-20 11:57:53 +0200

    Generated code: blade create -t service -p com.liferay.player.web.internal.info.collection.provider -c PlayerCollectionProvider -s com.liferay.info.collection.provider.InfoCollectionProvider -v 7.4 player-web

---
+++ modules/player-web/src/main/java/com/liferay/player/web/internal/info/collection/provider/PlayerCollectionProvider.java
@@ -0,0 +1,21 @@
+package com.liferay.player.web.internal.info.collection.provider;
+
+import com.liferay.info.collection.provider.InfoCollectionProvider;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author rubenpulido
+ */
+@Component(
+	immediate = true,
+	property = {
+		// TODO enter required service properties
+	},
+	service = InfoCollectionProvider.class
+)
+public class PlayerCollectionProvider implements InfoCollectionProvider {
+
+	// TODO enter required service methods
+
+}
