4aa1803002250de862bc04635a33098a633da107
Rubén Pulido <ruben.pulido@liferay.com>
2021-10-20 12:02:01 +0200

    SF

--- modules/player-web/src/main/java/com/liferay/player/web/internal/info/collection/provider/PlayerCollectionProvider.java
+++ modules/player-web/src/main/java/com/liferay/player/web/internal/info/collection/provider/PlayerCollectionProvider.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.player.web.internal.info.collection.provider;
 
 import com.liferay.info.collection.provider.InfoCollectionProvider;
@@ -5,17 +19,8 @@ import com.liferay.info.collection.provider.InfoCollectionProvider;
 import org.osgi.service.component.annotations.Component;
 
 /**
- * @author rubenpulido
+ * @author Rubén Pulido
  */
-@Component(
-	immediate = true,
-	property = {
-		// TODO enter required service properties
-	},
-	service = InfoCollectionProvider.class
-)
+@Component(immediate = true, service = InfoCollectionProvider.class)
 public class PlayerCollectionProvider implements InfoCollectionProvider {
-
-	// TODO enter required service methods
-
 }
